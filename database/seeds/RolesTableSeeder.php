74b704fa07fc5bc4568cbf98f6127622cc54f007
Rodrigo Cebrian <rodrigocebriang@gmail.com>
2020-03-20 17:29:44 +0100

    Roles migrations & seeders

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use \App\Models\Users\Role;
+use Illuminate\Database\Seeder;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create([
+            'id' => 1,
+            'type' => 'admin'
+        ]);
+
+        Role::create([
+            'id' => 2,
+            'type' => 'client'
+        ]);
+    }
+}
