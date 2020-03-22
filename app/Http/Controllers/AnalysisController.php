e70d5c21ecfe88ffaf6bacf99e881e622cb057bd
Jazmin <21750959@live.uem.es>
2020-03-22 16:44:03 +0100

    Usando exec en vez de Process
    Como hemos tenido problemas con los diferentes sistemas operativos hemos optado por usar exec.

--- app/Http/Controllers/AnalysisController.php
+++ app/Http/Controllers/AnalysisController.php
@@ -3,39 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Symfony\Component\Process\Exception\ProcessFailedException;
-use Symfony\Component\Process\Process;
-
 
 class AnalysisController extends Controller
 {
     function analyze(Request $request)
     {
-        $process = new Process(['../vendor/python/venv/bin/python', '../vendor/python/src/analysis.py', $request->getContent()]);
-        $process->run();
-
-        // executes after the command finishes
-        if (!$process->isSuccessful()) {
-            throw new ProcessFailedException($process);
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            exec('..\vendor\python\venv\Scripts\python ..\vendor\python\src\analysis.py ' . json_encode($request->getContent()), $output_array);
+            $response = json_decode($output_array[0], true);
+            return response($response, 200);
+        } else {
+            exec('../vendor/python/venv/bin/python ../vendor/python/src/analysis.py ' . json_encode($request->getContent()), $output_array);
+            $response = json_decode($output_array[0], true);
+            return response($response, 200);
         }
-
-        $response = json_decode($process->getOutput(), true);
-
-        return response($response, 200);
     }
 
     function translate(Request $request)
     {
-        $process = new Process(['../vendor/python/venv/bin/python', '../vendor/python/src/translate.py', $request->getContent()]);
-        $process->run();
-
-        // executes after the command finishes
-        if (!$process->isSuccessful()) {
-            throw new ProcessFailedException($process);
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            exec('..\vendor\python\venv\Scripts\python ..\vendor\python\src\translate.py ' . json_encode($request->getContent()), $output_array);
+            $response = json_decode($output_array[0], true);
+            return response($response, 200);
+        } else {
+            exec('../vendor/python/venv/bin/python ../vendor/python/src/translate.py ' . json_encode($request->getContent()), $output_array);
+            $response = json_decode($output_array[0], true);
+            return response($response, 200);
         }
-
-        $response = json_decode($process->getOutput(), true);
-
-        return response($response, 200);
     }
 }
