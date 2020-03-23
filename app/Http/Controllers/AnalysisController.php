0bcb55166955ec69adcf671f0c08c31096399a30
Rodrigo Cebrian <rodrigocebriang@gmail.com>
2020-03-23 17:08:53 +0100

    test para windows

--- app/Http/Controllers/AnalysisController.php
+++ app/Http/Controllers/AnalysisController.php
@@ -10,12 +10,11 @@ class AnalysisController extends Controller
 {
     function analyze(Request $request)
     {
-
         $cmd = config('python.exec');
-        $cript = config('python.scripts') . 'analysis.py';
-//        dd($cmd);
+        $script = config('python.scripts') . 'analysis.py';
+        $json = $request->json()->all();
 
-        $process = new Process([$cmd, $cript, $request->getContent()]);
+        $process = new Process([$cmd, $script, '{"lib": ' . $json['lib'] . ' ,"msg": "' . $json['msg'] . '"}']);
         $process->run();
 
         // executes after the command finishes
@@ -31,14 +30,15 @@ class AnalysisController extends Controller
 
     function translate(Request $request)
     {
-        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-            exec('..\vendor\python\venv\Scripts\python ..\vendor\python\src\translate.py ' . json_encode($request->getContent()), $output_array);
-            $response = json_decode($output_array[0], true);
-            return response($response, 200);
-        } else {
-            exec('../vendor/python/venv/bin/python ../vendor/python/src/translate.py ' . json_encode($request->getContent()), $output_array);
-            $response = json_decode($output_array[0], true);
-            return response($response, 200);
-        }
+        // get request content
+        $json = $request->json()->all();
+
+        // make the command
+        $cmd = config('python.exec') . ' ' . config('python.scripts') . 'translate.py ' . $json;
+        exec($cmd, $output_array);
+
+        // get response
+        $response = json_decode($output_array[0], true);
+        return response($response, 200);
     }
 }
