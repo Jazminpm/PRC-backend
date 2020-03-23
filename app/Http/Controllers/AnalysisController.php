558ff6f7d7e898dff45bf7e8a81c0621335db124
Rodrigo Cebrian <rodrigocebriang@gmail.com>
2020-03-23 14:09:32 +0100

    python scripts

--- app/Http/Controllers/AnalysisController.php
+++ app/Http/Controllers/AnalysisController.php
@@ -3,28 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 
 class AnalysisController extends Controller
 {
     function analyze(Request $request)
     {
-//        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-//            exec('..\vendor\python\venv\Scripts\python ..\vendor\python\src\analysis.py ' . json_encode($request->getContent()), $output_array);
-//            $response = json_decode($output_array[0], true);
-//            return response($response, 200);
-//        } else {
-//            exec('../vendor/python/venv/bin/python ../vendor/python/src/analysis.py ' . json_encode($request->getContent()), $output_array);
-//            $response = json_decode($output_array[0], true);
-//            return response($response, 200);
-//        }
 
-//        $cmd = storage_path('../vendor/python/venv/bin/python ../vendor/python/src/analysis.py ' . json_encode($request->getContent()));
-        dd(config('app.python'));
-//        $ans = shell_exec($cmd);
-//        dd($ans);
+        $cmd = config('python.exec');
+        $cript = config('python.scripts') . 'analysis.py';
+//        dd($cmd);
 
-        $response = json_decode($output_array[0], true);
+        $process = new Process([$cmd, $cript, $request->getContent()]);
+        $process->run();
+
+        // executes after the command finishes
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        $response = json_decode($process->getOutput(), true);
+
         return response($response, 200);
+
     }
 
     function translate(Request $request)
