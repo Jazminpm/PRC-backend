4b49b775f2acc32d6b698cfaeec9656ccb4e76a2
Rodrigo Cebrian <rodrigocebriang@gmail.com>
2020-03-23 13:47:25 +0100

    Python var

--- app/Http/Controllers/AnalysisController.php
+++ app/Http/Controllers/AnalysisController.php
@@ -8,15 +8,23 @@ class AnalysisController extends Controller
 {
     function analyze(Request $request)
     {
-        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-            exec('..\vendor\python\venv\Scripts\python ..\vendor\python\src\analysis.py ' . json_encode($request->getContent()), $output_array);
-            $response = json_decode($output_array[0], true);
-            return response($response, 200);
-        } else {
-            exec('../vendor/python/venv/bin/python ../vendor/python/src/analysis.py ' . json_encode($request->getContent()), $output_array);
-            $response = json_decode($output_array[0], true);
-            return response($response, 200);
-        }
+//        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+//            exec('..\vendor\python\venv\Scripts\python ..\vendor\python\src\analysis.py ' . json_encode($request->getContent()), $output_array);
+//            $response = json_decode($output_array[0], true);
+//            return response($response, 200);
+//        } else {
+//            exec('../vendor/python/venv/bin/python ../vendor/python/src/analysis.py ' . json_encode($request->getContent()), $output_array);
+//            $response = json_decode($output_array[0], true);
+//            return response($response, 200);
+//        }
+
+//        $cmd = storage_path('../vendor/python/venv/bin/python ../vendor/python/src/analysis.py ' . json_encode($request->getContent()));
+        dd(config('app.python'));
+//        $ans = shell_exec($cmd);
+//        dd($ans);
+
+        $response = json_decode($output_array[0], true);
+        return response($response, 200);
     }
 
     function translate(Request $request)
--- config/app.php
+++ config/app.php
@@ -1,6 +1,30 @@
 <?php
 
+function getPath() {
+    $path = storage_path('vendor' . DIRECTORY_SEPARATOR . 'python' . DIRECTORY_SEPARATOR . 'venv' . DIRECTORY_SEPARATOR);
+
+    if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+        $python_path = $path . 'Scripts' . DIRECTORY_SEPARATOR . 'python.exe';
+    } else {
+        $python_path = $path . 'bin' . DIRECTORY_SEPARATOR . 'python';
+    }
+    return $python_path;
+}
+
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Python exec path
+    |--------------------------------------------------------------------------
+    |
+    | This value is the path of the python environment. This value is used when
+    | the framework needs to execute a python script
+    |
+    */
+
+    'python' => getPath(),
+
 
     /*
     |--------------------------------------------------------------------------
