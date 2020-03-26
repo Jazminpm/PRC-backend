ab5c1d259307d7f3dcdedd7908153a06168093da
Rodrigo Cebrian <rodrigocebriang@gmail.com>
2020-03-26 22:43:48 +0100

    Twitter scraper via tweepy

---
+++ storage/app/python/src/scrapers/utils.py
@@ -0,0 +1,65 @@
+# -*- encoding: utf-8 -*-
+
+import json
+import re
+from datetime import datetime
+
+import tweepy
+
+consumer_key = "F2pIrutjymGr9vZuqTeViAymw"
+consumer_secret = "QFLTXwFJZNuR6i00IswAZIgaKsKl5AtmPufoSaRnR57ER2yVxS"
+access_token = "879408865997201408-QJDeVNKYBsTdp97caK0qFV454YYwRLp"
+access_token_secret = "gLNfeVRjT7LrDaWWSgCX0ZRu6TeTPBquTxlYKQ4hUzAka"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+
+def remove_emoji(msg):
+    """Remove emojis characters from an input str.
+
+    Note:
+        Code obtained from https://stackoverflow.com/questions/33404752/removing-emojis-from-a-string-in-python
+
+    Args:
+        msg (str): Message to remove emojis.
+
+    Returns:
+        Same msg (str) with removed emojis.
+    """
+    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  # emoticons
+                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               u"\U00002702-\U000027B0"
+                               u"\U000024C2-\U0001F251"
+                               "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', msg)
+
+
+def get_tweets_by_hashtag(hashtag, date, lang='en'):
+    """Get tweets from a day with a hashtag filter.
+
+    Notes:
+        The hashtag parameter is for search a city.
+
+    Args:
+        hashtag (str): some hashtag to search in twitter
+        date (str): date in YYYY-mm-dd format
+        lang (str):
+
+    """
+    query = '#' + hashtag + ' AND #travel OR #viaje -filter:retweets'
+    for tweet in tweepy.Cursor(api.search, q=query, since=date, lang=lang, tweet_mode='extended').items():
+        ee = {
+            'date': tweet.created_at.strftime("%Y-%m-%d"),
+            'comment': remove_emoji(tweet.full_text)
+        }
+        print(json.dumps(ee, ensure_ascii=False))
+
+
+if __name__ == "__main__":
+    today = datetime.now().strftime("%Y-%m-%d")
+    get_tweets_by_hashtag(hashtag='Madrid', date=today, lang='es')
