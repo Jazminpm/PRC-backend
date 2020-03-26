21789b77da7836cc3d0823fb1ccd5de91f9b9e9d
Rodrigo Cebrian <rodrigocebriang@gmail.com>
2020-03-26 13:06:39 +0100

    exec function

--- app/Http/Controllers/AnalysisController.php
+++ app/Http/Controllers/AnalysisController.php
@@ -3,27 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Symfony\Component\Process\Exception\ProcessFailedException;
-use Symfony\Component\Process\Process;
 
 class AnalysisController extends Controller
 {
     function analyze(Request $request)
     {
-        $cmd = config('python.exec');
-        $script = config('python.scripts') . 'analysis.py';
+        // get request content
         $json = $request->json()->all();
 
-        $process = new Process([$cmd, $script, '{"lib": ' . $json['lib'] . ' ,"msg": "' . $json['msg'] . '"}']);
-        $process->run();
+        // make the command
+        $cmd = config('python.exec') . ' ' . config('python.scripts') . 'analysis.py ' . json_encode($json);
+        exec($cmd, $output_array);
 
-        // executes after the command finishes
-        if (!$process->isSuccessful()) {
-            throw new ProcessFailedException($process);
-        }
-
-        $response = json_decode($process->getOutput(), true);
-
+        // get response
+        $response = json_decode($output_array[0], true);
         return response($response, 200);
 
     }
@@ -34,7 +27,7 @@ class AnalysisController extends Controller
         $json = $request->json()->all();
 
         // make the command
-        $cmd = config('python.exec') . ' ' . config('python.scripts') . 'translate.py ' . $json;
+        $cmd = config('python.exec') . ' ' . config('python.scripts') . 'translate.py ' . json_encode($json);
         exec($cmd, $output_array);
 
         // get response
