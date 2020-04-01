ce14e7975ebd81e90b48939ce28bc61449914969
Rodrigo Cebrian <rodrigocebriang@gmail.com>
2020-04-02 01:34:57 +0200

    INSERT weather data if possible. UPDATE otherwise.

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -7,10 +7,19 @@ use Illuminate\Support\Facades\DB;
 
 class WeatherController extends Controller
 {
+    /**
+     * INSERT weather data. UPDATE otherwise.
+     *
+     * @param Request $request json encoded data
+     * @return \Illuminate\Contracts\Routing\ResponseFactory code status
+     */
     function insert(Request $request)
     {
         $json = json_decode($request->getContent(), true);
-        DB::table('weathers')->updateOrInsert($json);
-        return response()->json($json, 200);
+        DB::table('weathers')->updateOrInsert([
+            'date_time' => $json['date_time'],
+            'airport_id' => $json['airport_id']
+        ], $json);
+        return response()->setStatusCode(200);
     }
 }
